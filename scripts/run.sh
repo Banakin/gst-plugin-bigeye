c190d032558a79c9328b54a93b90778d6bd38499
Ray Foxyote <ray@foxyote.com>
2025-11-05 01:17:36 -0500

    Clean up AI bs

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 # Run the UVC webcam source plugin with a simple test pipeline
 # This will capture from the webcam and display it on screen
 
-GST_PLUGIN_PATH=`pwd`/target/debug gst-launch-1.0 bigeyesrc ! jpegdec ! videoconvert ! autovideosink
+GST_PLUGIN_PATH=`pwd`/target/debug gst-launch-1.0 bigeyesrc ! queue ! jpegdec ! videoconvert ! autovideosink
